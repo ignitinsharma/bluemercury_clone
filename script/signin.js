6250b7e3455fc91296fcafd4e528a8fb8c7065d7
Rashmiranjan Mahanta <rashmiranjan007jb@gmail.com>
2022-09-29 12:44:42 +0530

    sign in / login page added with js and css files

---
+++ script/signin.js
@@ -0,0 +1,33 @@
+let signup_data= JSON.parse(localStorage.getItem('signup_data'))
+
+let login= document.getElementById('login')
+
+login.onclick= ()=>{
+    checkData()
+}
+
+function checkData(){
+    let d={
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value,
+    }
+    let flagE=false
+    let flagP=false
+
+    signup_data.forEach((el)=>{
+        if(el.email===d.email){
+            flagE=true
+            if(el.password===d.password){
+                flagP=true
+            }
+        }
+    })
+    if(flagE==true && flagP==true){
+        alert('Login successfull')
+        window.location.href='./index.html'
+    }else if(flagE==false ){
+        return alert('User Does Not Exists!')
+    }else if(flagP===false){
+        return alert('Wrong Credentials Provided!')
+    }
+}
