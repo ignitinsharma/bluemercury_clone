2c550b1be5de5a4a63f288f1430ebc63ffd0fe72
sumitkprasad123 <sumitkprasad123@gmail.com>
2022-09-29 23:45:42 +0530

    added home page

---
+++ script/index.js
@@ -0,0 +1,70 @@
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const navigationDots = document.querySelector(".navigation-dots");
+
+let numberOfImages = slideImage.length;
+let slideWidth = slideImage[0].clientWidth;
+let currentSlide = 0;
+
+// Set up the slider
+
+function init() {
+  /*   
+    slideImage[0] = 0
+    slideImage[1] = 100%
+    slideImage[2] = 200%
+    */
+
+  slideImage.forEach((img, i) => {
+    img.style.left = i * 100 + "%";
+  });
+
+  slideImage[0].classList.add("active");
+
+  createNavigationDots();
+}
+
+init();
+
+// Create navigation dots
+
+function createNavigationDots() {
+  for (let i = 0; i < numberOfImages; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("single-dot");
+    navigationDots.appendChild(dot);
+
+    dot.addEventListener("click", () => {
+      goToSlide(i);
+    });
+  }
+
+  navigationDots.children[0].classList.add("active");
+}
+
+// // Go To Slide
+
+function goToSlide(slideNumber) {
+  slidesContainer.style.transform =
+    "translateX(-" + slideWidth * slideNumber + "px)";
+
+  currentSlide = slideNumber;
+
+  setActiveClass();
+}
+
+// Set Active Class
+
+function setActiveClass() {
+  // Set active class for Slide Image
+
+  let currentActive = document.querySelector(".slide-image.active");
+  currentActive.classList.remove("active");
+  slideImage[currentSlide].classList.add("active");
+
+    // set active class for navigation dots
+
+  let currentDot = document.querySelector(".single-dot.active");
+  currentDot.classList.remove("active");
+  navigationDots.children[currentSlide].classList.add("active");
+}
