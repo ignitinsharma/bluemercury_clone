53b13124b97839638c8198262ef2f4754de59481
Sachin Pal <s2638070@gmail.com>
2022-09-30 00:46:44 +0530

    day-3

--- script/products.js
+++
@@ -1,9 +0,0 @@
-let products=[
-    {
-        img:"https://cdn.shopify.com/s/files/1/0283/0185/2747/products/global_images-810081492522-1_900x.jpg?v=1663939982",
-        brand:"R+CO BLEU",
-        title: "F-Layer Deep Conditioning Serum",
-        price: "F-Layer Deep Conditioning Serum",
-        badge: "NEW CONSCIOUS BEAUTY",
-    }
-]
