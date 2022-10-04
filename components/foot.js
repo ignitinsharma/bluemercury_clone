c71f2ba954ce04f394ea1dc3dcaf19d57a4926d0
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-10-04 10:22:03 +0530

    footer update

--- components/foot.js
+++ components/foot.js
@@ -1,114 +1,114 @@
 function foot(){
 
-    return `
-    
-    <div id="footer">
-      <div id="mainfooter">
-        <div id="socialmedia">
-          <p style="color: #5d6c85; font-weight: 550">
-            Get Expert Beauty Insight & Offers
-          </p>
-          <input id="emailbox" type="text" placeholder="EMAIL ADDRESS"">
-          <button id="emailbtn">---></button>
-          <p style="font-size: 0.9rem; color: #12284c">
-            By continuing, you agree to Bluemercury's
-            <span
-              ><a style="text-decoration: none" href=""
-                >Privacy Practices</a
-              ></span
-            >.
-          </p>
-        </div>
-
-        <div id="social_media_div2">
-          
-            <a href=""
-              ><i
-                class="fa fa-twitter"
-                style="font-size: 20px; color: #12284c"
-              ></i
-            ></a>
-
-            <a href=""
-              ><i
-                class="fa fa-pinterest"
-                style="font-size: 20px; color: #12284c"
-              ></i
-            ></a>
-
-            <a href=""
-              ><i
-                class="fa fa-facebook-f"
-                style="font-size: 20px; color: #12284c"
-              ></i
-            ></a>
-
-            <a href=""
-              ><i
-                class="fa fa-instagram"
-                style="font-size: 20px; color: #12284c"
-              ></i
-            ></a>
-
-            <a href=""
-              ><i
-                class="fa fa-youtube-play"
-                style="font-size: 20px; color: #12284c"
-              ></i
-            ></a>
-    
-        </div>
+  return `
+  
+  <div id="footer">
+    <div id="mainfooter">
+      <div id="socialmedia">
+        <p style="color: #5d6c85; font-weight: 550">
+          Get Expert Beauty Insight & Offers
+        </p>
+        <input id="emailbox" type="text" placeholder="EMAIL ADDRESS"">
+        <button id="emailbtn">---></button>
+        <p style="font-size: 0.9rem; color: #12284c">
+          By continuing, you agree to Bluemercury's
+          <span
+            ><a style="text-decoration: none" href=""
+              >Privacy Practices</a
+            ></span
+          >.
+        </p>
       </div>
 
-      <div id="gridpro">
-        <div id="Productsdiv">
-          <ul>
-            <h4 style="color: #495a75">BLUEMERCURY</h4>
-            <li><a href="">Our Company</a></li>
-            <li><a href="">Store Locator</a></li>
-            <li><a href="">In-Store & Online Events</a></li>
-            <li><a href="">Spa Menu</a></li>
-            <li><a href="">Careers</a></li>
-            <li><a href="">Vendor Submissions</a></li>
-            <li><a href="">Diversity & Inclusion</a></li>
-          </ul>
-        </div>
+      <div id="social_media_div2">
+        
+          <a href=""
+            ><i
+              class="fa fa-twitter"
+              style="font-size: 20px; color: #12284c"
+            ></i
+          ></a>
 
-        <div id="Productsdiv">
-          <ul>
-            <h4 style="color: #495a75">MY ACCOUNT</h4>
-            <li><a href="">My Account</a></li>
-            <li><a href="">BlueRewards</a></li>
-            <li><a href="">Gift Card Balance Checker</a></li>
-          </ul>
-        </div>
+          <a href=""
+            ><i
+              class="fa fa-pinterest"
+              style="font-size: 20px; color: #12284c"
+            ></i
+          ></a>
 
-        <div id="Productsdiv">
-          <ul>
-            <h4 style="color: #495a75">CUSTOMER SERVICE</h4>
-            <li><a href="">Contact Us</a></li>
-            <li><a href="">Help & FAQ</a></li>
-            <li><a href="">Shipping, Returns & Exchanges</a></li>
-            <li><a href="">Gift Cards</a></li>
-            <li><a href="">Klarna</a></li>
-          </ul>
-        </div>
+          <a href=""
+            ><i
+              class="fa fa-facebook-f"
+              style="font-size: 20px; color: #12284c"
+            ></i
+          ></a>
 
-        <div id="Productsdiv">
-          <ul>
-            <h4 style="color: #495a75">RESOURCES</h4>
-            <li><a href="">Terms & Conditions</a></li>
-            <li><a href="">Privacy Notice</a></li>
-            <li><a href="">Do Not Sell My Personal Information</a></li>
-            <li><a href="">CA Privacy Rights</a></li>
-            <li><a href="">Accessibility</a></li>
-            <li><a href="">Affiliate Program</a></li>
-          </ul>
-        </div>
+          <a href=""
+            ><i
+              class="fa fa-instagram"
+              style="font-size: 20px; color: #12284c"
+            ></i
+          ></a>
+
+          <a href=""
+            ><i
+              class="fa fa-youtube-play"
+              style="font-size: 20px; color: #12284c"
+            ></i
+          ></a>
+  
       </div>
     </div>
 
-    `
+    <div id="gridpro">
+      <div id="Productsdiv">
+        <ul>
+          <h4 style="color: #495a75">BLUEMERCURY</h4>
+          <li><a href="./ourcompany.html">Our Company</a></li>
+          <li><a href="">Store Locator</a></li>
+          <li><a href="">In-Store & Online Events</a></li>
+          <li><a href="">Spa Menu</a></li>
+          <li><a href="">Careers</a></li>
+          <li><a href="">Vendor Submissions</a></li>
+          <li><a href="">Diversity & Inclusion</a></li>
+        </ul>
+      </div>
+
+      <div id="Productsdiv">
+        <ul>
+          <h4 style="color: #495a75">MY ACCOUNT</h4>
+          <li><a href="">My Account</a></li>
+          <li><a href="">BlueRewards</a></li>
+          <li><a href="">Gift Card Balance Checker</a></li>
+        </ul>
+      </div>
+
+      <div id="Productsdiv">
+        <ul>
+          <h4 style="color: #495a75">CUSTOMER SERVICE</h4>
+          <li><a href="">Contact Us</a></li>
+          <li><a href="">Help & FAQ</a></li>
+          <li><a href="">Shipping, Returns & Exchanges</a></li>
+          <li><a href="">Gift Cards</a></li>
+          <li><a href="">Klarna</a></li>
+        </ul>
+      </div>
+
+      <div id="Productsdiv">
+        <ul>
+          <h4 style="color: #495a75">RESOURCES</h4>
+          <li><a href="">Terms & Conditions</a></li>
+          <li><a href="">Privacy Notice</a></li>
+          <li><a href="">Do Not Sell My Personal Information</a></li>
+          <li><a href="">CA Privacy Rights</a></li>
+          <li><a href="">Accessibility</a></li>
+          <li><a href="">Affiliate Program</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  `
 }
 
 export {foot};
